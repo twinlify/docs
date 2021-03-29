795cc13325d087dda7f6b3b405ea7f8ce178928d
chrisdugne <chris.dugne@uralys.com>
2021-03-29 21:26:08 +0200

    using common global style

--- src/index.js
+++ src/index.js
@@ -7,29 +7,11 @@ import styled, {createGlobalStyle} from 'styled-components';
 
 import Views from './views';
 import Header from './components/header';
-import {primary, ReloadAnchor, ScrollToTop} from '@twinlify/walrus';
+import {globalStyle, ReloadAnchor, ScrollToTop} from '@twinlify/walrus';
 
 // -----------------------------------------------------------------------------
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: #ededed;
-    font-size: 17px;
-    font-family: Poppins;
-  }
-
-  a {
-    color: ${primary};
-    font-weight: 500;
-    text-decoration: none;
-
-    &:hover {
-      color: #444;
-    }
-  }
-`;
+const GlobalStyle = createGlobalStyle`${globalStyle}`;
 
 const $Site = styled.div`
   display: flex;
