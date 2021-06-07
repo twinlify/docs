16ccb38061e1b54f54c763dc2cc9a12e3fd0b9c9
chrisdugne <chris.dugne@uralys.com>
2021-06-07 14:34:55 +0200

    added sitemap generation

--- reactor.config.js
+++ reactor.config.js
@@ -8,5 +8,16 @@ module.exports = {
   documentation: {
     dist: './tocs',
     source: './src/pages/docs'
+  },
+  sitemap: {
+    hostname: 'https://docs.twinlify.com',
+    links: [
+      {url: '/', lastmod: new Date()},
+      {url: '/documentation/api/', lastmod: new Date()},
+      {url: '/documentation/getting-started/', lastmod: new Date()},
+      {url: '/documentation/using-bundler/', lastmod: new Date()},
+      {url: '/documentation/using-cdn/', lastmod: new Date()},
+      {url: '/documentation/changelog/', lastmod: new Date()}
+    ]
   }
 };
