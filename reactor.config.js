ea7b6cba5bbadc5a898783070ad99ac4b83a6fcb
chrisdugne <chris.dugne@uralys.com>
2021-06-11 19:54:53 +0200

    testing sitemap update

--- reactor.config.js
+++ reactor.config.js
@@ -14,11 +14,11 @@ module.exports = {
     hostname: 'https://docs.twinlify.com',
     links: [
       {url: '/', lastmod: new Date()},
-      {url: '/documentation/api/', lastmod: new Date()},
+      {url: '/documentation/api', lastmod: new Date()},
       {url: '/documentation/getting-started/', lastmod: new Date()},
       {url: '/documentation/using-bundler/', lastmod: new Date()},
       {url: '/documentation/using-cdn/', lastmod: new Date()},
-      {url: '/documentation/changelog/', lastmod: new Date()}
+      {url: '/documentation/changelog', lastmod: new Date()}
     ]
   }
 };
